5c4f0a5b46b3e67d8ae3078ee8fb8d88cef89e8c
KNR1997 <kethakaranasinghe@gmail.com>
2026-05-05 20:02:42 +0530

    feat(serializer): Subject related Response, Create and Update serializers

--- academy/app/serializers/subject.py
+++ academy/app/serializers/subject.py
@@ -2,13 +2,19 @@ from academy.db.models import Subject
 from .base import BaseSerializer
 
 
-class SubjectSerializer(BaseSerializer):
-    class Meta:
-        model = Subject
-        fields = '__all__'
-
-
-class SubjectListSerializer(BaseSerializer):
+class SubjectResponseSerializer(BaseSerializer):
     class Meta:
         model = Subject
         fields = ['id', 'name', 'slug', 'code']
+
+
+class SubjectCreateSerializer(BaseSerializer):
+    class Meta:
+        model = Subject
+        fields = ['name', 'slug', 'code']
+
+
+class SubjectUpdateSerializer(BaseSerializer):
+    class Meta:
+        model = Subject
+        fields = ['name', 'slug', 'code']

64a94da6b02b6f21f32d6a4a3881b49d1580ede7
KNR1997 <kethakaranasinghe@gmail.com>
2026-06-24 22:56:26 +0530

    feat: Enrollment derivable fields commented

--- academy/db/models/enrollment.py
+++ academy/db/models/enrollment.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 # Django imports
 from django.db import models
 
@@ -11,10 +13,10 @@ class EnrollmentStatusType(models.TextChoices):
 
 
 class Enrollment(BaseModel):
-    status = models.CharField(max_length=20, choices=EnrollmentStatusType.choices)
-    last_payment_month = models.PositiveIntegerField(default=0)
-    last_payment_year = models.PositiveIntegerField(default=0)
-    is_active = models.BooleanField(default=False)
+    # status = models.CharField(max_length=20, choices=EnrollmentStatusType.choices)
+    # last_payment_month = models.PositiveIntegerField(default=0)
+    # last_payment_year = models.PositiveIntegerField(default=0)
+    # is_active = models.BooleanField(default=False)
 
     student = models.ForeignKey(
         'db.Student', related_name="enrollments", on_delete=models.SET_NULL, null=True
@@ -23,8 +25,24 @@ class Enrollment(BaseModel):
         'db.CourseOffering', related_name="enrollments", on_delete=models.SET_NULL, null=True
     )
 
+    @property
+    def last_payment_date(self):
+        last_paid = self.charges.filter(status='paid').order_by(
+            '-billing_year', '-billing_month').first()
+        if last_paid:
+            return (last_paid.billing_year, last_paid.billing_month)
+        return None
+
+    @property
+    def is_active(self):
+        if not self.last_payment_date:
+            return False
+        year, month = self.last_payment_date
+        current_year, current_month = datetime.now().year, datetime.now().month
+        return (year > current_year) or (year == current_year and month >= current_month)
+
     class Meta:
         db_table = "enrollment"
 
-    def __str__(self):
-        return self.status
+    # def __str__(self):
+    #     return self.course_offering.

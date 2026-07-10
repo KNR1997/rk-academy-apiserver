0ef3b2acacb53a1fe1b5c0a57fc434e1ba937e7d
KNR1997 <kethakaranasinghe@gmail.com>
2026-07-10 14:06:09 +0530

    feat: Remove Subject slug unique, enable code unique

--- academy/db/models/subject.py
+++ academy/db/models/subject.py
@@ -7,8 +7,8 @@ from .base import BaseModel
 
 class Subject(BaseModel):
     name = models.CharField(max_length=20, unique=True)
-    code = models.CharField(max_length=20)
-    slug = models.SlugField(max_length=20, unique=True)
+    code = models.CharField(max_length=20, unique=True)
+    slug = models.SlugField(max_length=20, null=True, blank=True)
 
     class Meta:
         db_table = "subject"
